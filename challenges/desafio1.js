393abdcffe9616e01234090bf40da8fac8b76139
Rebeca Cerqueira <rcks.santos@gmail.com>
2021-01-22 16:17:09 -0300

    corrige diversos erros de Lint quanto a espaços e vírgulas

--- challenges/desafio1.js
+++ challenges/desafio1.js
@@ -1,8 +1,8 @@
-/* Inclua o campo criadoPor em todos os documentos, colocando "Ronald McDonald" 
+/* Inclua o campo criadoPor em todos os documentos, colocando "Ronald McDonald"
 no valor desse campo.
 Para isso, escreva no arquivo desafio1.js duas queries, nesta ordem:
 
-1)Crie uma query que adicione o campo criadoPor em todos os documentos, 
+1)Crie uma query que adicione o campo criadoPor em todos os documentos,
 colocando "Ronald McDonald"
 no valor desse campo.
 
@@ -12,9 +12,9 @@ db.products.updateMany(
   {},
   {
     $set: {
-      criadoPor: "Ronald McDonald"
-    }
-  }
+      criadoPor: "Ronald McDonald",
+    },
+  },
 );
 
 db.products.find({}, { nome: 1, criadoPor: 1, _id: 0 });
