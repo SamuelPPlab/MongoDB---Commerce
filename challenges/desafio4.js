be9e440b0ad459c54638d2c8c5c2cfb66199b302
Marcelo Pessini <marceloppessini@gmail.com>
2021-01-22 16:09:40 -0300

    desafio

--- challenges/desafio4.js
+++ challenges/desafio4.js
@@ -1,13 +1,2 @@
-db.customers.updateOne(
-  { nome: "Big Mac" },
-  { $currentDate: {
-    ultimaModificacao: true,
-    "cancellation.date": { $type: "timestamp" },
-  },
-  $set: {
-    "cancellation.reason": "user request",
-    status: "D",
-  },
-  },
-);
+db.customers.updateOne({ nome: "Big Mac" }, { $currentDate: { ultimaModificacao: true } });
 db.produtos.find({}, { nome: 1, ultimaModificacao: 1, _id: 0 });
