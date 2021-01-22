2c8d962d89768ff8fcc47f6768591ea082bb2f04
Dilenio Enderle <dilenio@gmail.com>
2021-01-22 16:16:23 -0300

    feat: challenge 5

--- challenges/desafio3.js
+++ challenges/desafio3.js
@@ -5,19 +5,15 @@ db.produtos.updateMany(
 
 db.produtos.updateMany(
   { tags: { $all: ["bovino"] } },
-  {
-    $inc: { avaliacao: 5 }
-  }
+  { $inc: { avaliacao: 5 } },
 );
 
 db.produtos.updateMany(
   { tags: { $all: ["ave"] } },
-  {
-    $inc: { avaliacao: 3 }
-  }
+  { $inc: { avaliacao: 3 } },
 );
 
 db.produtos.find(
   {},
-  { nome: 1, avaliacao: 1, _id: 0}
+  { nome: 1, avaliacao: 1, _id: 0 },
 );
