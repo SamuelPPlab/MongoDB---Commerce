becd7b466292fc488a1eb3ac2ac232110ac079fc
jederson <jmacedo.ti@gmail.com>
2021-01-22 16:24:37 -0300

    Desafio 5 fixing

--- challenges/desafio5.js
+++ challenges/desafio5.js
@@ -1,7 +1,6 @@
 db.produtos.updateMany(
-  { $not: { name: "McChicken" } },
+  { nome: { $ne: "McChicken" } },
   { $addToSet: { ingredientes: "ketchup" } },
-  { $upsert: true },
 );
 
 db.produtos.find({}, { nome: 1, ingredientes: 1, _id: 0 });
