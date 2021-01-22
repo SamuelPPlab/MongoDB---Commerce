9e1f2c3e9a2bc778100a0486d0a470181cf23624
Kesley Lahoz <namielahoz@gmail.com>
2021-01-22 17:00:29 -0300

    challenge 5

--- challenges/desafio5.js
+++ challenges/desafio5.js
@@ -1,3 +1,3 @@
 db.produtos.update({ nome: { $ne: "McChicken" } }, { $addToSet: { ingredientes: "ketchup" } });
 
-db.produtos.find({}, { nome: 1, ingredientes: 1, _id: 0 });
+db.produtos.find({}, { nome: 1, ingredientes: 1, _id: 0 }).pretty();
