0dcb19608183b7c3abcf66a192bddbf466e0e45d
Cássio Rubens <cassiorubensadv@gmail.com>
2021-01-22 19:13:37 -0300

    FEAT: Desafio 01

--- challenges/desafio1.js
+++ challenges/desafio1.js
@@ -5,3 +5,14 @@
 // Crie uma query que adicione o campo criadoPor em todos os documentos,
 // colocando "Ronald McDonald" no valor desse campo.
 // Crie uma query que retorne o nome e criadoPor de todos os produtos.
+db.produtos.updateMany(
+  {},
+  { $set:
+    { criadoPor: "Ronald McDonald" },
+  },
+);
+
+db.produtos.find(
+  {},
+  { _id: 0, nome: 1, criadoPor: 1 },
+);
