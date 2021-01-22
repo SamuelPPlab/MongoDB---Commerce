cb2de1a48bca05143ff18b89fffb97f693ce3d10
Victor Bergamini <victor.bergamini@loldesign.com.br>
2021-01-22 17:32:10 -0300

    req4-fix

--- challenges/desafio4.js
+++ challenges/desafio4.js
@@ -1,2 +1,2 @@
-db.produtos.updateOne({ nome: "Big Mac" }, { $set: { ultimaModificacao: new Date() } }, { $upsert: 1 });
+db.produtos.updateOne({ nome: "Big Mac" }, { $set: { ultimaModificacao: new Date() } }, { $upsert: true });
 db.produtos.find({ ultimaModificacao: { $exists: true } }, { nome: 1, _id: 0 });
