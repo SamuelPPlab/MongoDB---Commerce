c0476f112b9ea7a4d831145633a743a5450ed9e8
Marcelo Pessini <marceloppessini@gmail.com>
2021-01-22 17:31:50 -0300

    desafio

--- challenges/desafio12.js
+++ challenges/desafio12.js
@@ -1,2 +1,2 @@
-db.produtos.updateMany({}, { valoresNutricionais: { $sort: { percentual: -1 } } });
+db.produtos.updateMany({}, { valoresNutricionais: { $each, $sort: { percentual: -1 } } });
 db.produtos.find({}, { nome: 1, valoresNutricionais: 1, _id: 0 });
