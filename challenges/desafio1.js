e5b9abca61ab323c2a417a46f28844dc7923078a
Lucas Santiago Gomide <lucas.sgomide92@gmail.com>
2021-01-22 15:54:04 -0300

    Fixing first challenge linter issues

--- challenges/desafio1.js
+++ challenges/desafio1.js
@@ -1,13 +1,9 @@
-// Crie uma query que adicione o campo criadoPor em todos os documentos, colocando "Ronald McDonald" no valor desse campo.
-
-// Crie uma query que retorne o nome e criadoPor de todos os produtos.
-
 db.produtos.updateMany({}, {
   $set: {
-    criadoPor: "Ronald McDonald"
-  }
+    criadoPor: "Ronald McDonald",
+  },
 });
 
 db.produtos.find({}, {
-  _id: false, nome: true, criadoPor: true
+  _id: false, nome: true, criadoPor: true,
 });
