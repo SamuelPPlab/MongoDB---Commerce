24fbe97fb9efeab16130ef3c24d760b84563da7b
HCaetano <hrcgeo@gmail.com>
2021-01-22 18:56:14 -0300

    Add challenges 12 through 16

---
+++ challenges/desafio14.js
@@ -0,0 +1,20 @@
+db.produtos.updateMany(
+  {
+    valoresNutricionais: {
+      $elemMatch: {
+        percentual: { $gt: 20, $lt: 40 },
+        tipo: "sódio",
+      },
+    },
+  },
+  {
+    $push: {
+      tags: "contém sódio",
+    },
+  },
+);
+
+db.produtos.find(
+  {},
+  { _id: 0, nome: 1, tags: 1 },
+).pretty();
