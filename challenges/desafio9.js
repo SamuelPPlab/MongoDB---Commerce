e2b90bf08061687ab9d84b6bd3940ba92b2948e0
bynho25 <pablo.trybe@gmail.com>
2021-01-22 19:17:31 -0300

    Fix

--- challenges/desafio9.js
+++ challenges/desafio9.js
@@ -1,6 +1,6 @@
 db.produtos.updateOne(
   { nome: "Cheddar McMelt" },
-  { $pop: { ingrediente: 1 } },
+  { $pop: { ingredientes: 1 } },
 );
 
-db.produtos.find({}, { _id: 0, nome: 1, ingrediente: 1 });
+db.produtos.find({}, { _id: 0, nome: 1, ingredientes: 1 });
