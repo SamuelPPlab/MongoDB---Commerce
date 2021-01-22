af96a079ffcc216a0d7239508230513de7a2ca46
William <williamjog@hotmail.com>
2021-01-22 18:58:46 -0300

    Tentando 14º Requisito

--- challenges/desafio14.js
+++ challenges/desafio14.js
@@ -1,7 +1,7 @@
 db.produtos.updateMany(
   { tags: {
     $elemMatch:
-      { tipo: "sódio", percentual: { $and: [{ $gt: 20 }, { $lt: 40 }] } },
+      { tipo: "sódio", percentual: { $gt: 20, $lt: 40 } },
   } },
 );
 
