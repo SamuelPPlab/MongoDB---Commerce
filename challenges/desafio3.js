17f2c641e84b5bd2c89a674e99b0ba98778e0716
Marcelo Pessini <marceloppessini@gmail.com>
2021-01-22 16:14:18 -0300

    desafio

--- challenges/desafio3.js
+++ challenges/desafio3.js
@@ -1,4 +1,4 @@
 db.produtos.updateMany({}, { $set: { avaliacao: NumberInt("0") } });
 db.produtos.updateMany({ "tags.0": "bovino" }, { $inc: { avaliacao: 5 } });
-db.produtos.updateMany({ "tags.1": "ave" }, { $inc: { avaliacao: 3 } });
+db.produtos.updateMany({ "tags.0": "ave" }, { $inc: { avaliacao: 3 } });
 db.produtos.find({}, { nome: 1, avaliacao: 1, _id: 0 });
