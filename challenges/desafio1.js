99e7acbce5779a58e100e022fcadb0dad569530a
RenatoCostaDev <renatotrybe@gmail.com>
2021-01-22 16:55:27 -0300

    fix:Requirement-1 and 2

--- challenges/desafio1.js
+++ challenges/desafio1.js
@@ -1,1 +1,2 @@
 db.produtos.updateMany({}, { $set: { criadoPor: "Ronald McDonald" } }, { _id: 0, nome: 1, criadoPor: 1 });
+db.produtos.find({}, { _id: 0, nome: 1, criadoPor: 1 }).pretty();
--- challenges/desafio2.js
+++ challenges/desafio2.js
@@ -1,1 +1,2 @@
 db.produtos.updateMany({ valorUnitario: { $exists: false } }, { $set: { valorUnitario: NumberDecimal("0.00") } }, { _id: 0, nome: 1, valorUnitarior: 1 });
+db.produtos.find({}, { _id: 0, nome: 1, valorUnitario: 1 }).pretty();
