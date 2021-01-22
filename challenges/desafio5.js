ec12e9f9bff05ce6dd4328ff9daf5e1e489641ab
kissyla <kissylamottapereira@gmail.com>
2021-01-22 19:20:13 -0300

    desafio 7

--- challenges/desafio5.js
+++ challenges/desafio5.js
@@ -1,2 +1,2 @@
-db.produtos.updateMany({ nome: { $ne: "McChicken" } }, { $addToSet: { ingredientes: "ketchup" }} );
+db.produtos.updateMany({ nome: { $ne: "McChicken" } }, { $addToSet: { ingredientes: "ketchup" } });
 db.produtos.find({ }, { nome: true, ingredientes: true, _id: false }).pretty();
