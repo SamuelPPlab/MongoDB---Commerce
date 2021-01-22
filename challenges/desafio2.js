824d13f58ddf9f9d4a46ab36e166c5a5bdd998b0
Júlia Pôrto <juportob@gmail.com>
2021-01-22 18:51:14 -0300

    Desafio 2 - fix code

--- challenges/desafio2.js
+++ challenges/desafio2.js
@@ -1,6 +1,5 @@
-db.produtos.updateMany({ valorUnitario: { $exists: 0 } },
-  { $set: { valorUnitario: NumberDecimal(0.00) } },
-  { $upsert: true });
+db.produtos.updateMany({},
+  { $set: { valorUnitario: NumberDecimal("0.00") } });
 
 db.produtos.find({},
   { nome: 1, valorUnitario: 1, _id: 0 });
