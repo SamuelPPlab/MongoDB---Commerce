ec90dbb432617c39cf146fd1ca9e0d580d69e3eb
Isa Koga <isadorakoga710@gmail.com>
2021-01-22 16:44:27 -0300

    :rotating-light: Fix challenge 5

--- challenges/desafio5.js
+++ challenges/desafio5.js
@@ -1,3 +1,3 @@
 db.produtos.updateMany({ nome: { $ne: "McChicken" } }, { $addToSet: { ingredientes: "ketchup" } });
 
-db.produtos.find({}, { nome: 1, ultimaModificacao: 1, _id: 0 });
+db.produtos.find({}, { nome: 1, ingredientes: 1, _id: 0 });
