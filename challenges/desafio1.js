4c5bbc646b4162750a62c271fee5d5e4fe9de457
Cyrano webdev <cyrano@cyranowebdev.com>
2021-01-22 16:23:44 -0300

    Desafios 1 a 5.

--- challenges/desafio1.js
+++ challenges/desafio1.js
@@ -2,7 +2,7 @@ db.produtos.updateMany(
   {},
   { $set: {
     criadoPor: "Ronald McDonald",
-  }},
+  } },
 );
 
 db.produtos.find({}, { nome: 1, criadoPor: 1 });
---
+++ challenges/desafio4.js
@@ -0,0 +1,8 @@
+db.produtos.updateOne(
+  { nome: "Big Mac" },
+  { $set: {
+    ultimaModificacao: new Date(),
+  } },
+);
+
+db.produtos.find({ ultimaModificacao: { $exists: true } }, { nome: 1 });
