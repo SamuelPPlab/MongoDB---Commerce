a952534b81a96926736b5851c43e07dc6ee485d7
William <williamjog@hotmail.com>
2021-01-22 19:20:58 -0300

    Tentando 14º Requisito 5.0

--- challenges/desafio14.js
+++ challenges/desafio14.js
@@ -1,9 +1,7 @@
 db.produtos.updateMany(
-  { valoresNutricionais: {
-    $elemMatch:
-      { tipo: "sódio" },
-    $and: [{ percentual: { $gt: 20 } }, { percentual: { $lt: 40 } }] },
-  },
+  { valoresNutricionais:
+    { $elemMatch: { tipo: "sódio" }, $and: [{ percentual: { $gt: 20 } }, { percentual: { $lt: 40 } }] } },
+  { $push: { tags: { $each: ["contém sódio"] } } },
 );
 
 db.produtos.find(
