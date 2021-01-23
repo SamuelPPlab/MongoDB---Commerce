00efe43fdd2ba9133bd16b9da6884ffee3d5a00f
VitorSSMoreira <vitorssmoreira@gmail.com>
2021-01-22 22:42:25 -0300

    challenge 4

---
+++ challenges/desafio4.js
@@ -0,0 +1,7 @@
+db.produtos.updateMany(
+  { nome: { $eq: "Big Mac" } },
+  { $set: { ultimaModificacao: Date() } },
+);
+
+db.produtos.find({ ultimaModificacao: { $exists: true } },
+  { nome: 1, _id: 0 });
