12399a13fc87c8472c9f248fc291cf35c8a73362
Rebeca Cerqueira <rcks.santos@gmail.com>
2021-01-23 10:06:52 -0300

    desafio18 -sim, agora sim o desafio 18

--- challenges/desafio18.js
+++ challenges/desafio18.js
@@ -1,3 +1,15 @@
 /*
+Conte quantos documentos contêm a expressão feito com utilizando o operador $text.
 
+Para isso, escreva no arquivo desafio18.js duas queries, nesta ordem:
+
+1)Crie uma query que faça a criação de um índice do tipo text no campo descricao
+com o idioma padrão portuguese.
+
+2)Crie uma query que retorne a quantidade de documentos que contêm a expressão
+feito com utilizando o operador $text.
 */
+
+db.produtos.createIndex({ descricao: "text" }, { default_language: "portuguese" });
+
+db.produtos.count({ $text: { $search: "\"feito com\"" } });
