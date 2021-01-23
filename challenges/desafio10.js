e4137b1419d4e94c8fdbf48bfd6cc93524752974
kissyla <kissylamottapereira@gmail.com>
2021-01-23 15:00:41 -0300

    codeclimate desafio 11

--- challenges/desafio10.js
+++ challenges/desafio10.js
@@ -1,4 +1,4 @@
-db.produtos.updateMany({ }, { $push: { "vendasPorDias": { $each: [0, 0, 0, 0, 0, 0, 0] } } });
+db.produtos.updateMany({ }, { $push: { vendasPorDias: { $each: [0, 0, 0, 0, 0, 0, 0] } } });
 db.produtos.updateOne({ nome: "Big Mac" }, { $inc: { "vendasPorDias.3": 60 } });
-db.produtos.updateMany({ $and: [{ tags: "bovino"}, {tags: "pão" }] }, { "vendasPorDias.6": { $inc: 120 } });
+db.produtos.updateMany({ $and: [{ tags: "bovino" }, { tags: "pão" }] }, { "vendasPorDias.6": { $inc: 120 } });
 db.produtos.find({ }, { nome: true, vendasPorDia: true, _id: false });
