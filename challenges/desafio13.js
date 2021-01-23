d9e7949d37c63dba68eea497aab0e615dd5c77cf
Rodrigo Leite <rods.leite27@gmail.com>
2021-01-23 19:40:26 -0300

    eslint challenges 10-14

--- challenges/desafio13.js
+++ challenges/desafio13.js
@@ -1,5 +1,5 @@
 db.produtos.updateMany(
   { valoresNutricionais: { $elemMatch: { tipo: "sódio", percentual: { $gt: 40 } } } },
-  { $push: { tags: "muito sódio" } }
+  { $push: { tags: "muito sódio" } },
 );
 db.produtos.find({}, { _id: 0, nome: 1, tags: 1 });
