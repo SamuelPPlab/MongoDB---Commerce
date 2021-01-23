ba4b191a0b4a068971f234046e871bb98f31bf4a
Rodrigo Leite <rods.leite27@gmail.com>
2021-01-23 18:12:36 -0300

    challenge 4 fix 2º attempt

--- challenges/desafio4.js
+++ challenges/desafio4.js
@@ -1,5 +1,2 @@
-db.produtos.updateOne({ nome: "Big Mac" }, { $currentDate: { ultimaModificacao: ISOdate() } });
-db.produtos.find(
-  { ultimaModificacao: { $exists: true } },
-  { _id: 0, nome: 1, ultimaModificacao: 1 },
-);
+db.produtos.updateOne({ nome: "Big Mac" }, { $set: { ultimaModificacao: ISODate() } });
+db.produtos.find({ ultimaModificacao: { $exists: true } }, { _id: 0, nome: 1 });
