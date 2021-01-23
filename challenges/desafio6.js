7cb55652bb92ab47b9a728df1358f60a5e4e75d5
Rodrigo Leite <rods.leite27@gmail.com>
2021-01-23 18:34:18 -0300

    challenge 6, 7, 8

--- challenges/desafio6.js
+++ challenges/desafio6.js
@@ -1,2 +1,5 @@
-db.produtos.updateMany({ nome: { $exists: ["McChicken", "Quarteirão com Queijo"] } }, { $addToSet: { ingredientes: "ketchup" } });
-// db.produtos.find({ }, { _id: 0, nome: 1, ingredientes: 1 });
+db.produtos.updateMany(
+  { $or: [{ nome: "Big Mac" }, { nome: "Quarteirão com Queijo" }] },
+  { $addToSet: { ingredientes: "bacon" } },
+);
+db.produtos.find({}, { _id: 0, nome: 1, ingredientes: 1 });
