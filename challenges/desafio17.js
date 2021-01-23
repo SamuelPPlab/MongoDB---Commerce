1abc18d907a50119cae50aed57a42ad0b97efd91
Rebeca Cerqueira <rcks.santos@gmail.com>
2021-01-23 10:04:19 -0300

    desafio17 e desafio18

---
+++ challenges/desafio17.js
@@ -0,0 +1,16 @@
+/*
+Conte quantos documentos contêm as palavras frango e hamburguer utilizando o
+operador $text.
+
+Para isso, escreva no arquivo desafio17.js duas queries, nesta ordem:
+
+1)Crie uma query que faça a criação de um índice do tipo text no campo
+descricao com o idioma padrão portuguese.
+
+2)Crie uma query que retorne a quantidade de documentos que contêm as palavras
+frango e hamburguer utilizando o operador $text.
+
+*/
+
+db.produtos.createIndex({ descricao: "text" }, { default_language: "portuguese" });
+db.produtos.count({ $text: { $search: "frango hamburguer" } });
