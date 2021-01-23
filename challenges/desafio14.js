b3fdf7f25e42a2b19ddcde5b09769f692cafddf4
STEFANO-BRANZ <stefano.branz@gmail.com>
2021-01-22 21:33:29 -0300

    Requirement 13 and 14 - push 02 fixed first param with elemMatch

--- challenges/desafio14.js
+++ challenges/desafio14.js
@@ -1,6 +1,9 @@
 db.produtos.updateMany(
-  { "valoresNutricionais.tipo": "sódio",
-    "valoresNutricionais.percentual": { $gt: 20, $lt: 40 },
+  { valoresNutricionais: {
+    $elemMatch: {
+      tipo: "sódio", percentual: { $gt: 20, $lt: 40 },
+    },
+  },
   },
   {
     $push: { tags: "contém sódio" },
