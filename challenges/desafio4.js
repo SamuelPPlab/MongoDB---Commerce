9c0c5e7955f3917439cf66aaa02da6d02f33c767
deko possas <andrepossaster@gmail.com>
2021-01-22 23:09:58 -0300

    lint

--- challenges/desafio4.js
+++ challenges/desafio4.js
@@ -1,2 +1,2 @@
-db.produtos.updateMany( { nome: "Big Mac" }, { $set: { ultimaModificacao: new Date() } }, { $upsert: true } );
-db.produtos.find({ ultimaModificacao: { $exists: true } }, {nome: 1, _id: 0});
+db.produtos.updateMany({ nome: "Big Mac" }, { $set: { ultimaModificacao: new Date() } }, { $upsert: true });
+db.produtos.find({ ultimaModificacao: { $exists: true } }, { nome: 1, _id: 0 });
