3a930bee9f7781bc83ecdb71bf3e8fd3cb4dbf9b
Aloízio Borges Santos Coelho <aloiziobsc@gmail.com>
2021-01-23 20:08:06 -0300

    :art: Reqs 1 ate 17
     1 ate 17

---
+++ challenges/desafio7.js
@@ -0,0 +1,4 @@
+db.produtos.updateMany({ ingredientes: { $in: ["cebola"] } },
+  { $pull: { ingredientes: { $in: ["cebola"] } } });
+db.produtos.find({},
+  { _id: 0, nome: 1, ingredientes: 1 }).pretty();
