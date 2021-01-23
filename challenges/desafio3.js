efe7cccdb80defdd2ef58d4b7a242a113b75b3df
rafMewtwo <ponciano.raf@gmail.com>
2021-01-22 21:20:45 -0300

    desafio3.7

--- challenges/desafio3.js
+++ challenges/desafio3.js
@@ -6,14 +6,14 @@ db.produtos.updateMany(
 );
 
 db.produtos.updateMany(
-  { $elemMatch: { tag: "bovino" },
+  { $elemMatch: { tag: "bovino" } },
   { $set:
     { avaliacao: NumberInt("5") },
   },
 );
 
 db.produtos.updateMany(
-  { $elemMatch: { tag: "bovino" },
+  { $elemMatch: { tag: "bovino" } },
   { $set:
     { avaliacao: NumberInt("3") },
   },
