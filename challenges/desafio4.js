514127a14ae6c0ac2812767f8526680f277445ff
rafMewtwo <ponciano.raf@gmail.com>
2021-01-22 21:53:23 -0300

    desafio4

---
+++ challenges/desafio4.js
@@ -0,0 +1,13 @@
+db.produtos.updateOne(
+  { nome: "Big Mac" },
+  { $currentDate:
+    {
+    ultimaModificacao: true
+    }
+  }
+);
+
+db.produtos.find(
+  { ultimaModificacao: { $exist: true } },
+  { nome: true, _id: false}
+);
