ea6a0b0f810df21c2568dfc1667667bbf2340a15
STEFANO-BRANZ <stefano.branz@gmail.com>
2021-01-22 21:05:10 -0300

    Requirement 13 - push 01

---
+++ challenges/desafio13.js
@@ -0,0 +1,17 @@
+db.produtos.updateMany(
+  { "valoresNutricionais.tipo": "sódio",
+    "valoresNutricionais.percentual": { $gte: 40 },
+  },
+  {
+    $push: { tags: "muito sódio" },
+  },
+);
+
+db.produtos.find(
+  {},
+  {
+    _id: 0,
+    nome: 1,
+    tags: 1,
+  },
+).pretty();
