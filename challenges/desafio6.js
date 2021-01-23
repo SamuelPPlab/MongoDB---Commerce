b7e25c8c1afbced094977f54e35bfdc60da1f603
Kesley Lahoz <namielahoz@gmail.com>
2021-01-23 16:24:40 -0300

    challenge 6

--- challenges/desafio6.js
+++ challenges/desafio6.js
@@ -1,3 +1,3 @@
 db.produtos.updateMany({ nome: { $in: ["Big Mac", "McChicken"] } }, { $push: { ingredientes: "Bacon" } });
 
-db.produtos.find({}, { nome: 1, _id: 0, ingredientes: 1 }).pretty();
+db.produtos.find({}, { nome: 1, ingredientes: 1, _id: 0 }).pretty();
