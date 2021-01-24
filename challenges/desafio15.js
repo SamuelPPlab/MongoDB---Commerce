45e4c66c5e15f808ef4d9e85c21b22e8a0245ca9
rafMewtwo <ponciano.raf@gmail.com>
2021-01-24 00:22:45 -0300

    desafio15.4

--- challenges/desafio15.js
+++ challenges/desafio15.js
@@ -1,3 +1,3 @@
 // db.produtos.find({ nome: /mc/i }).count();
-db.artists.count({ nome: { $regex: /Mc/i } });
+db.artists.find({ nome: { $regex: /Mc/i } }).count();
 // db.artists.count({ nome: /Mc/i });
