e7eea61dd5418106cb96d08eef9e17706dec4a30
rafMewtwo <ponciano.raf@gmail.com>
2021-01-24 16:50:24 -0300

    desafio20.1

--- challenges/desafio20.js
+++ challenges/desafio20.js
@@ -1,7 +1,7 @@
 db.produtos.updateMany(
   { nome: { $eq: "Big Mac" } },
-  { 
-    $unset: { 
+  {
+    $unset: {
       curtidas: "",
     },
   },
@@ -9,5 +9,5 @@ db.produtos.updateMany(
 
 db.produtos.find(
   {},
-  { _id: 0, nome: 1, curtidas: 1},
+  { _id: 0, nome: 1, curtidas: 1 },
 );
