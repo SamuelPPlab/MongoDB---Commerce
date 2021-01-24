53289db5602918ea48ad5790dd41b9c3ce1e64be
Júlia Pôrto <juportob@gmail.com>
2021-01-24 12:25:32 -0300

    Desafio 3 - fix code

--- challenges/desafio3.js
+++ challenges/desafio3.js
@@ -2,10 +2,10 @@ db.produtos.updateMany({},
   { $set: { avaliacao: NumberInt(0) } });
 
 db.produtos.updateMany({ tags: "bovino " },
-  { $inc: { avaliacao: 5 } });
+  { $inc: { avaliacao: NumberInt(5) } });
 
 db.produtos.updateMany({ tags: "ave " },
-  { $inc: { avaliacao: 3 } });
+  { $inc: { avaliacao: NumberInt(3) } });
 
 db.produtos.find({},
   { nome: 1, avaliacao: 1, _id: 0 });
