d410f32c78c035f067a6a149f39d6518a02aa77e
rodrigoandrademelo <rodrigoandrademelo@gmail.com>
2021-01-24 20:20:56 -0300

    req 03

--- challenges/desafio1.js
+++ challenges/desafio1.js
@@ -7,3 +7,8 @@ db.produtos.find(
   {},
   { nome: 1, criadoPor: 1, _id: 0 },
 );
+
+// referencias
+// https://docs.mongodb.com/manual/tutorial/model-monetary-data/
+// https://stackoverflow.com/questions/8218484/mongodb-inserts-float-when-trying-to-insert-integer
+// https://docs.mongodb.com/manual/reference/method/db.collection.updateMany/
