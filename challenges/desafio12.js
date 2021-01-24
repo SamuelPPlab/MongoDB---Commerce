3336b90cc835e13033a0aab8c286bb93795f8ea7
rafMewtwo <ponciano.raf@gmail.com>
2021-01-23 21:43:49 -0300

    desafio12

---
+++ challenges/desafio12.js
@@ -0,0 +1,13 @@
+db.produtos.updateMany(
+  {},
+  // valoresNutricionais: dentro do push que faz ordenar valores nutricionais
+  { $push: { valoresNutricionais:
+    { $each: [], $sort { percentual: -1 } }, 
+  } },
+);
+
+db.produtos.find(
+  {},
+  { nome: 1, _id: 0, valoresNutricionais: 1 },
+);
+
