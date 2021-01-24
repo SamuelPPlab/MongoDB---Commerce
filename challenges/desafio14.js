c4ea300ec87d9054ad938e0b872e1aca472af57c
kissyla <kissylamottapereira@gmail.com>
2021-01-24 08:41:04 -0300

    codeclimate 2

--- challenges/desafio14.js
+++ challenges/desafio14.js
@@ -3,7 +3,7 @@ db.produtos.updateMany({ "valoresNutricionais.tipo": "sódio" },
     [
       { "valoresNutricionais.percentual": { $gte: 20 } },
       { "valoresNutricionais.percentual": { $lte: 40 } },
-    ]
+    ],
   },
   { $push: { valoresNutricionais: { $each: ["contém sódio"] } } });
 db.produtos.find({ }, { nome: true, tags: true, _id: false });
