a2a85e03af582d8f70075a3d5d2aaafe29097867
rafMewtwo <ponciano.raf@gmail.com>
2021-01-23 23:22:36 -0300

    desafio14.3

--- challenges/desafio14.js
+++ challenges/desafio14.js
@@ -1,9 +1,16 @@
-db.produtos.updatesMany(
-  { valoresNutricionais:
-    { $elemMatch: { tipo: "sódio", percentual: { $gt: 20, $lt: 40 } } },
+// db.produtos.updatesMany(
+//   { valoresNutricionais:
+//     { $elemMatch: { tipo: "sódio", percentual: { $gt: 20, $lt: 40 } } },
+//   },
+//   { $push: { tags: "contém sódio" } },
+// );
+db.produtos.updateMany({
+  valoresNutricionais: { $elemMatch: { tipo: "sódio", percentual: { $gt: 20, $lt: 40 } } },
+}, {
+  $push: {
+    tags: "contém sódio",
   },
-  { $push: { tags: "contém sódio" } },
-);
+});
 
 db.produtos.find(
   {},
