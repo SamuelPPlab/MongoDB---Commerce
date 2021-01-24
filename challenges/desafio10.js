7ed34f78ff1b4fdbc175ea4ce63e7c503013b2d1
kissyla <kissylamottapereira@gmail.com>
2021-01-24 20:14:09 -0300

    eslint desafio 10

--- challenges/desafio10.js
+++ challenges/desafio10.js
@@ -1,5 +1,5 @@
 db.produtos.updateMany({ },
   { $push: { vendasPorDia: { $each: [0, 0, 0, 0, 0, 0, 0] } } });
 db.produtos.updateOne({ nome: "Big Mac" }, { $inc: { "vendasPorDia.3": 60 } });
-db.produtos.updateMany({ tags: { $in: ["bovino", "pão"] } }, { $inc: {  "vendasPorDia.6": 120 } });
+db.produtos.updateMany({ tags: { $in: ["bovino", "pão"] } }, { $inc: { "vendasPorDia.6": 120 } });
 db.produtos.find({ }, { nome: true, vendasPorDia: true, _id: false });
--- challenges/desafio12.js
+++ challenges/desafio12.js
@@ -1,2 +1,4 @@
-db.produtos.updateMany({ }, { valoresNutricionais: { $each: [] }, $sort: { percentual: -1 } });
+db.produtos.updateMany({ }, { $push: { valoresNutricionais: { $each: [] } }, $sort: { percentual: -1 } });
 db.produtos.find({ }, { nome: true, valoresNutricionais: true, _id: false });
+
+{ $push: { <field>: { $each: [ <value1>, <value2> ... ] } } }
