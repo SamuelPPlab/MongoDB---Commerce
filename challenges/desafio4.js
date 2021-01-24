efed6772c262a3fbe303e5e7912197764576f703
Daniel Menescal <danielmjales@bct.ect.ufrn.br>
2021-01-24 17:11:24 -0300

    add desafio4 js

---
+++ challenges/desafio4.js
@@ -0,0 +1,9 @@
+db.products.updateOne(
+  { nome: "Big Mac" },
+  { $currentDate: { $ultimaModificacao: true } },
+);
+
+db.products.find(
+  { ultimaModificacao: { $exists: 1 } },
+  { nome: 1, _id: 0 },
+);
