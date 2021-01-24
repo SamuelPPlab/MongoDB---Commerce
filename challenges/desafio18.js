ac11148bf10ea0b1df82a8e105af8ed2b29d63de
rafMewtwo <ponciano.raf@gmail.com>
2021-01-24 18:46:01 -0300

    desafio 18.2

--- challenges/desafio18.js
+++ challenges/desafio18.js
@@ -1,3 +1,3 @@
 db.produtos.createIndex({ descricao: "text" }, { default_language: "portuguese" });
 
-db.produtos.find({ $text: { $search: "\"feito com\"" } });
+db.produtos.count({ $text: { $search: "\"feito com\"" } });
