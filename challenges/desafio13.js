02b759fe101f50c9422e8de192bd6f17e9218ab6
rafMewtwo <ponciano.raf@gmail.com>
2021-01-23 23:35:10 -0300

    desafio13.3

--- challenges/desafio13.js
+++ challenges/desafio13.js
@@ -2,7 +2,7 @@ db.produtos.updateMany(
   {
     valoresNutricionais: { $elemMatch: { tipo: "sódio", percentual: { $gte: 40 } } },
   },
-  { push: { tags: "muito sódio" } },
+  { $push: { tags: "muito sódio" } },
 );
 
 db.produtos.find(
