1c142c9b17b63bb14358066006b4c695c6c23b9f
kissyla <kissylamottapereira@gmail.com>
2021-01-24 17:30:36 -0300

    codeclimate 2

--- challenges/desafio10.js
+++ challenges/desafio10.js
@@ -1,6 +1,5 @@
 db.produtos.updateMany({ },
   { $push: { vendasPorDia: { $each: [0, 0, 0, 0, 0, 0, 0] } } });
-db.produtos.updateOne({ nome: "Big Mac" },
-  { $inc: { "vendasPorDia.3": 60 } });
+db.produtos.updateOne({ nome: "Big Mac" }, { $inc: { "vendasPorDia.3": 60 } });
 db.produtos.updateMany({ $and: [{ tags: "bovino" }, { tags: "pão" }] }, { "vendasPorDia.6": { $inc: 120 } });
 db.produtos.find({ }, { nome: true, vendasPorDia: true, _id: false });
--- challenges/desafio14.js
+++ challenges/desafio14.js
@@ -2,8 +2,8 @@ db.produtos.updateMany(
   { valoresNutricionais:
     { $elemMatch:
       { percentual: { $gte: 20, $lte: 40 }, tipo: "sódio" },
-    }
+    },
   },
-  { $push: { tags: { $each:["contém sódio"] } } });
-db.produtos.find({ }, { nome: true, tags: true, _id: false }
-  );
+  { $push: { tags: { $each:["contém sódio"] } } }
+);
+db.produtos.find({ }, { nome: true, tags: true, _id: false });
