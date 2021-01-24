2a2e12cb8593fb90def5a0231a9b7d9830aa1218
kissyla <kissylamottapereira@gmail.com>
2021-01-24 20:19:24 -0300

    eslint de novo 3

--- challenges/desafio12.js
+++ challenges/desafio12.js
@@ -1,5 +1,5 @@
 db.produtos.updateMany({ },
   { $push: { valoresNutricionais: { $each: [] } },
-  $sort: { percentual: -1 } }
-);
+    $sort: { percentual: -1 }
+  });
 db.produtos.find({ }, { nome: true, valoresNutricionais: true, _id: false });
