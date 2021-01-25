d3e0710f5555755b32a280abaf27b514f0cac7d3
rodrigoandrademelo <rodrigoandrademelo@gmail.com>
2021-01-24 22:26:23 -0300

    req 12

--- challenges/desafio12.js
+++ challenges/desafio12.js
@@ -1,3 +1,11 @@
-// db.voos.count({
-//   "aeroportoDestino.pais": { $in: ["BRASIL", "ARGENTINA", "CHILE"] },
-// });
+db.produtos.updateMany(
+  {},
+  { $push: {
+    valoresNutricionais: { $each: [], $sort: -1 },
+  } },
+);
+
+db.produtos.find(
+  {},
+  { nome: 1, valoresNutricionais: 1, _id: 0 },
+);
