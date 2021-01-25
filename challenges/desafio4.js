c44b27f21301635e54d6b9cbb84eafbc5837bbf9
vapatty6848 <vapatty@gmail.com>
2021-01-25 09:06:39 -0300

    correçao desafio 4

--- challenges/desafio4.js
+++ challenges/desafio4.js
@@ -1,2 +1,6 @@
-db.produtos.updateOne({ nome: "Big Mac" }, { $currentDate: { ultimaModificacao: { $type: "date" } } });
-db.produtos.find({}, { _id: 0, nome: 1, ultimaModificacao: 1 });
+db.produtos.updateOne(
+  { nome: "Big Mac" },
+  { $currentDate: { ultimaModificacao: { $type: "date" } } },
+);
+db.produtos.find({ ultimaModificacao: { $exists: true } },
+  { _id: 0, nome: 1 });
