2c2295319860a2a73f5a06521a954d4cf8d70632
Paulo <paulocheloni@gmail.com>
2021-01-25 14:57:45 -0300

    adding req  22

--- challenges/desafio22.js
+++ challenges/desafio22.js
@@ -1,1 +1,1 @@
-db.produtos.find({ $expr: "this.produtos.vendidos % 5 === 0" }, { nome: 1, _id: 0, vendidos: 1 });
+db.produtos.find({ vendidos: { $mod: [5, 0] } }, { nome: 1, _id: 0, vendidos: 1 });
