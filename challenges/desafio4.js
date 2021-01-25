a60d50c9e5ef642f48b35007d2f44e4285754762
Júlia Pôrto <juportob@gmail.com>
2021-01-25 09:31:51 -0300

    Desafio 4 e 5

--- challenges/desafio4.js
+++ challenges/desafio4.js
@@ -1,3 +1,3 @@
-db.produtos.update({ nome: "Big Mac" }, { $currentDate: { ultimaModificacao: true } });
+db.produtos.update({ nome: "Big Mac" }, { $currentDate: { ultimaModificacao: { $type: "date" } } });
 
-db.find({ ultimaModificacao: { $exists: true } }, { _id: 0, nome: 1 });
+db.produtos.find({ ultimaModificacao: { $exists: true } }, { _id: 0, nome: 1 });
