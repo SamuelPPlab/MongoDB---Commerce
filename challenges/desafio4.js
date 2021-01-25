cd8f17b0ca3f5ac67d0e43c63e87b4a4f7ce3fca
Júlia Pôrto <juportob@gmail.com>
2021-01-25 09:23:41 -0300

    Desafio 4 - fix code

--- challenges/desafio4.js
+++ challenges/desafio4.js
@@ -1,5 +1,3 @@
-db.produtos.update({ nome: "Big Mac" },
-  { $1currentDate: { ultimaModificacao: true } });
+db.produtos.update({ nome: "Big Mac" }, { $currentDate: { ultimaModificacao: true } });
 
-db.find({ ultimaModificacao: { $exists: true } },
-  { _id: 0, nome: 1 });
+db.find({ ultimaModificacao: { $exists: true } }, { _id: 0, nome: 1 });
