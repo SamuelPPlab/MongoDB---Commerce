6186f6bb05c6c7492c0a7216388cbbe9c4925940
luizmacmillan <luizmacmillan@gmail.com>
2021-01-26 10:30:34 -0300

    Correçao do Desafio 4.

--- challenges/desafio4.js
+++ challenges/desafio4.js
@@ -1,3 +1,3 @@
-db.produtos.updateOne({ nome: { $eq: "Big Mac" } }, { $currentDate: { ultimaModificacao: 1 } });
+db.produtos.updateOne({ nome: "Big Mac" }, { $currentDate: { ultimaModificacao: 1 } });
 
 db.produtos.find({ ultimaModificacao: { $exists: 1 } }, { _id: 0, nome: 1 });
