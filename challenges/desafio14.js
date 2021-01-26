0591585a619855e9cd503d70c01df176b24bb471
rodrigoandrademelo <rodrigoandrademelo@gmail.com>
2021-01-25 23:01:50 -0300

    req 14

--- challenges/desafio14.js
+++ challenges/desafio14.js
@@ -1,1 +1,15 @@
-// db.voos.count({ "aeroportoOrigem.pais": { $ne: "BRASIL" } });
+db.produtos.updateMany(
+  { valoresNutricionais: {
+    $elemMatch: {
+      tipo: "sódio", percentual: { $gt: 20, $lt: 40 },
+    },
+  } },
+  { $push: {
+    tags: "contém sódio",
+  } },
+);
+
+db.produtos.find(
+  {},
+  { nome: 1, tags: 1, _id: 0 },
+);
