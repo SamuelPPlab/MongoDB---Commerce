9961e812581e424f7e8f3b207a11210d64128aa8
marioduartedev <marioduarte.engenheiro@gmail.com>
2021-01-26 11:50:14 -0300

    Desafio 14 feito

---
+++ challenges/desafio14.js
@@ -0,0 +1,7 @@
+db.produtos.updateMany(
+  { valoresNutricionais:
+    { $elemMatch:
+      { tipo: "sódio", $and: [{ percentual: { $gt: 20 } }, { percentual: { $lt: 40 } }] } } },
+  { $push: { tags: "contém sódio" } },
+);
+db.produtos.find({}, { nome: 1, _id: 0, tags: 1 });
