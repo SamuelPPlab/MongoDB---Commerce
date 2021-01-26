b56674744c44c51c2fadfdb10abd16e8be1763a7
luizmacmillan <luizmacmillan@gmail.com>
2021-01-26 11:07:26 -0300

    Desafio 14.

--- challenges/desafio13.js
+++ challenges/desafio13.js
@@ -1,6 +1,6 @@
 db.produtos.updateMany(
-{ valoresNutricionais: { $elemMatch: { tipo: "sódio", percentual: { $gte: 40 } } } },
-{ $addToSet: { tags: "muito sódio" } },
+  { valoresNutricionais: { $elemMatch: { tipo: "sódio", percentual: { $gte: 40 } } } },
+  { $addToSet: { tags: "muito sódio" } },
 );
 
 db.produtos.find({}, { _id: 0, nome: 1, tags: 1 });
