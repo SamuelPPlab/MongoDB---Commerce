130c1dd442df6e4029feb9635a37fe26ab5e8638
Isabela Rosa <isabelarosa954@gmail.com>
2021-01-26 01:19:40 -0300

    desafio 4 - teste

---
+++ challenges/desafio4.js
@@ -0,0 +1,10 @@
+db.produtos.updateOne(
+  { nome: "Big Mac" },
+  { $currentDate: { ultimaModificacao: { $type: "date" } } },
+  { $upsert: true },
+);
+
+db.produtos.find(
+  { ultimaModificacao: { $exists: "true" } },
+  { nome: 1, ultimaModificacao: 1, _id: 0 },
+);
