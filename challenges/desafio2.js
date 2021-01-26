69d210b5d51da438a8859ef95854f21f954ca5a9
Pedro Duarte <ikbenpedro@gmail.com>
2021-01-26 11:48:37 -0300

    desafio2

---
+++ challenges/desafio2.js
@@ -0,0 +1,8 @@
+db.produtos.updateMany({ valorUnitario: { $exists: false } },
+  {
+    $set: {
+      valorUnitario: "0.00",
+    },
+  });
+
+db.produtos.find({}, { nome: 1, valorUnitario: 1, _id: 0 });
