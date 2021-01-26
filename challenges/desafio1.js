b4d929f22ad9fdd6927094f37f81db870eb33875
Pedro Duarte <ikbenpedro@gmail.com>
2021-01-26 10:54:08 -0300

    desafio 1, atomic operator

--- challenges/desafio1.js
+++ challenges/desafio1.js
@@ -1,7 +1,8 @@
-db.commerce.updateMany(
-  { $set: {
-    criadoPor: "Ronald McDonald",
-  } },
-);
+db.commerce.updateMany({},
+  {
+    $set: {
+      criadoPor: "Ronald Mcdonald",
+    },
+  });
 
 db.commerce.find({}, { nome: 1, criadoPor: 1, _id: 0 });
