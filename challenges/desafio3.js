64510ffff4ef3631445ad094acb67b03c397bd62
Pedro Duarte <ikbenpedro@gmail.com>
2021-01-26 14:16:47 -0300

    desafio3

--- challenges/desafio3.js
+++ challenges/desafio3.js
@@ -1,21 +1,21 @@
 db.produtos.updateMany({},
   {
     $set: {
-      avaliacao: NumberInt("0"),
+      avaliacao: NumberInt(0),
     },
   });
 
 db.produtos.updateMany({ tags: ["bovino"] },
   {
     $inc: {
-      avaliacao: NumberInt("5"),
+      avaliacao: (5),
     },
   });
 
 db.produtos.updateMany({ tags: ["ave"] },
   {
     $inc: {
-      avaliacao: NumberInt("3"),
+      avaliacao: (3),
     },
   });
 
