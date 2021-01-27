f62328e4da1e058771ae102641b7d781a5b541e0
Matheuscara <matheuscara2000@gmail.com>
2021-01-27 15:57:00 -0300

    Lint

--- challenges/desafio6.js
+++ challenges/desafio6.js
@@ -1,10 +1,10 @@
 db.produtos.updateMany({
-    nome: {
-        $in: ["Big Mac", "Quarteirão com Queijo"]
-    }}, {
-    $addToSet: {
-        ingredientes: "bacon"
-    }
+  nome: {
+    $in: ["Big Mac", "Quarteirão com Queijo"],
+  } }, {
+  $addToSet: {
+    ingredientes: "bacon",
+  },
 });
 
 db.produtos.find({}, { _id: 0, nome: 1, ingredientes: 1 });
