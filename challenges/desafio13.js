5246dc38c94f557ee1b11d1ba44f282058043ca4
Pedro Eduardo <pedroedu77@gmail.com>
2021-01-27 19:15:11 -0300

    Desafio 11 ao 15 concluidos lintnovamente

--- challenges/desafio13.js
+++ challenges/desafio13.js
@@ -1,7 +1,7 @@
-db.produtos.updateMany({ 
+db.produtos.updateMany({
   valoresNutricionais:
   { $elemMatch:
-    { tipo: "sódio" ,  percentual: { $gte: 40 } },
-  }
-}, { $addToSet: { tags :"muito sódio" } } );
+    { tipo: "sódio", percentual: { $gte: 40 } },
+  },
+}, { $addToSet: { tags: "muito sódio" } });
 db.produtos.find({}, { nome: 1, tags: 1, _id: 0 });
--- challenges/desafio14.js
+++ challenges/desafio14.js
@@ -1,8 +1,7 @@
-db.produtos.updateMany({ 
-    valoresNutricionais:
+db.produtos.updateMany({
+  valoresNutricionais:
     { $elemMatch:
-      { tipo: "sódio" ,  percentual: { $lt: 40, $gt: 20 } },
+      { tipo: "sódio",  percentual: { $lt: 40, $gt: 20 } },
     }
-  }, { $addToSet: { tags :"contém sódio" } } );
-  db.produtos.find({}, { nome: 1, tags: 1, _id: 0 });
-  
+}, { $addToSet: { tags: "contém sódio" } });
+db.produtos.find({}, { nome: 1, tags: 1, _id: 0 });
