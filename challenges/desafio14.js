cba9ad0ca1ac9ff0404d8aa947acca376cda9212
ricknois <rickssti@outlook.com>
2021-01-27 14:24:23 -0300

    :bug: Fix files

--- challenges/desafio14.js
+++ challenges/desafio14.js
@@ -1,5 +1,6 @@
-db.produtos.updateMany({
-  valoresNutricionais: { $elemMatch: { tipo: "sódio", percentual: { $in: [20, 40] } } },
-  $addToSet: { tags: "contém sódio" } });
+db.produtos.updateMany(
+  { valoresNutricionais: { $elemMatch: { tipo: "sódio", percentual: { $lt: 40, $gt: 20 } } } },
+  { $addToSet: { tags: "contém sódio" } },
+);
 
-db.produtos.find({}, { nome: 1, tags: 1, _id: 0 });
+db.produtos.find({ }, { nome: 1, tags: 1, _id: 0 });
