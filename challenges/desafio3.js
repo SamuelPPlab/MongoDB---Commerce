19d2e8a74ca9977f3e9de24f47c38ce0076cc991
VinicinBraga <vinicius@bragamatta.com.br>
2021-01-27 16:36:35 -0300

    Desafios 1 ao 18

---
+++ challenges/desafio3.js
@@ -0,0 +1,11 @@
+db.produtos.updateMany({},
+  { $set: { avaliacao: NumberInt("0") } });
+
+db.produtos.updateMany({ tags: { $all: ["bovino"] } },
+  { $set: { avaliacao: NumberInt("5") } });
+
+db.produtos.updateMany({ tags: { $all: ["ave"] } },
+  { $inc: { avaliacao: 3 } });
+
+db.produtos.find({},
+  { nome: 1, avaliacao: 1, _id: 0 });
