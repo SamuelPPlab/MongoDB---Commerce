66ce14e646fb3d81a6a659c5bab8e7dbe7121486
Pedro Eduardo <pedroedu77@gmail.com>
2021-01-27 17:26:01 -0300

    Desafios 1 ao 6 lint

--- challenges/desafio6.js
+++ challenges/desafio6.js
@@ -1,2 +1,2 @@
-db.produtos.updateMany({ nome: { $in: ["Big Mac", "Quarteirão com Queijo"]  } },  { $addToSet: { ingredientes: "bacon" } } );
+db.produtos.updateMany({ nome: { $in: ["Big Mac", "Quarteirão com Queijo"] } },  { $addToSet: { ingredientes: "bacon" } });
 db.produtos.find({}, { nome: 1, ingredientes: 1, _id: 0 });
