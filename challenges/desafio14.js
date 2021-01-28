e0f106e07c2fcef2903ca3995e89db652f7b6ca3
Bruna Franciele <brunafranciele1902@gmail.com>
2021-01-28 19:11:46 -0300

    fix: requisito 14

--- challenges/desafio14.js
+++ challenges/desafio14.js
@@ -1,4 +1,5 @@
-db.produtos.updateMany({ $and: [{ "valoresNutricionais.percentual": { $gt: 20 } },
+db.produtos.updateMany({ $and: [{ "valoresNutricionais.tipo": "sódio" },
+  { "valoresNutricionais.percentual": { $gt: 20 } },
   { "valoresNutricionais.percentual": { $lt: 40 } }] },
 { $push: { tags: { $each: ["contém sódio"] } } });
 
