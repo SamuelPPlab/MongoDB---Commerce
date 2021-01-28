1a04326a5eec1f36532f8ef7e4915a543aaa6368
Alessandra <itsalematos@hotmail.com>
2021-01-28 17:56:41 -0400

    feat: 13,14

---
+++ challenges/desafio14.js
@@ -0,0 +1,4 @@
+db.produtos.updateMany({ $and: [{ "valoresNutricionais.percentual": { $gt: 20 } }, { "valoresNutricionais.percentual": { $lt: 40 } }] },
+  { $push: { tags: { $each: ["contém sódio"] } } });
+
+db.produtos.find({}, { nome: 1, tags: 1, _id: 0 });
