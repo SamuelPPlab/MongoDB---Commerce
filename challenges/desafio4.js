6e7623bcad09fc859ddbf4bb6abe1db1b10cfe1b
Rodrigo Santana <rodrigodesantanaribeiro@gmail.com>
2021-01-28 13:55:13 -0300

    space line

--- challenges/desafio4.js
+++ challenges/desafio4.js
@@ -1,2 +1,3 @@
 db.produtos.updateMany({ nome: "Big Mac" }, { $currentDate: { ultimaModificacao: { $type: "date" } } });
+
 db.produtos.find({ ultimaModificacao: { $exists: 1 } }, { nome: 1, _id: 0 });
