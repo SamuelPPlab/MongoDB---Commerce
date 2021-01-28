86ac8cb864ed292ef596788b3d66bdae916ddb89
Alessandra <itsalematos@hotmail.com>
2021-01-28 18:19:06 -0400

    fix: 13, 14

--- challenges/desafio14.js
+++ challenges/desafio14.js
@@ -1,4 +1,6 @@
-db.produtos.updateMany({ $and: [{ "valoresNutricionais.tipo": "sódio" }, { "valoresNutricionais.percentual": { $gt: 20 } }, { "valoresNutricionais.percentual": { $lt: 40 } }] },
-  { $push: { tags: { $each: ["contém sódio"] } } });
+db.produtos.updateMany(
+  { valoresNutricionais: { $elemMatch: { tipo: "sódio", percentual: { $gt: 20, $lt: 40 } } } },
+  { $push: { tags: "contém sódio" } },
+);
 
 db.produtos.find({}, { nome: 1, tags: 1, _id: 0 });
