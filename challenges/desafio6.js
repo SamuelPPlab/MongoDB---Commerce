b8fd3810b9ef50ee643e631b742d6dc1677a4706
Alessandra <itsalematos@hotmail.com>
2021-01-28 16:44:18 -0400

    fix: 6

--- challenges/desafio6.js
+++ challenges/desafio6.js
@@ -1,2 +1,3 @@
-db.produtos.update({ $or: [{ nome: "Big Mac" }, { nome: "Quarteirão com Queijo" }] }, { $addToSet: { ingredientes: "bacon" } });
+db.produtos.updateMany({ $or: [{ nome: "Big Mac" }, { nome: "Quarteirão com Queijo" }] }, { $addToSet: { ingredientes: "bacon" } });
+
 db.produtos.find({}, { nome: 1, ingredientes: 1, _id: 0 });
