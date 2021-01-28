0bf78c478094d081002645dc6419f47aca5ab328
Rodrigo Santana <rodrigodesantanaribeiro@gmail.com>
2021-01-28 11:56:08 -0300

    desafio3.js

---
+++ challenges/desafio3.js
@@ -0,0 +1,20 @@
+db.produtos.updateMany(
+    {},
+    { $set: { avaliacao: NumberInt(0) } },
+  );
+  
+  db.produtos.updateMany(
+    { tags: { $all: ["bovino"] } },
+    { $inc: { avaliacao: 5 } },
+  );
+  
+  db.produtos.updateMany(
+    { tags: { $all: ["ave"] } },
+    { $inc: { avaliacao: 3 } },
+  );
+  
+  db.produtos.find(
+    {},
+    { _id: 0, avaliacao: 1, nome: 1 },
+  );
+  
