69e524284863d0ddd77cc03900a78718d3a5ef90
Daniel Menescal <danielmjales@bct.ect.ufrn.br>
2021-01-28 18:18:45 -0300

    add desafio12 js, fix desafio3 js

---
+++ challenges/desafio12.js
@@ -0,0 +1,14 @@
+db.produtos.updateMany(
+  { },
+  { $push: {
+      valoresNutrionais: {
+        $each: [], $sort: { percentual: -1 },
+      },
+    },
+  },
+);
+
+db.produtos.find(
+  { },
+  { nome: 1, valoresNutrionais: 1, _id: 0 },
+);
