f5a5b2e8cbb21315315bc101c567c9943e71ec37
Anderson Iarrocheski <anderson.iarrocheski@fohat.co>
2021-01-28 19:48:27 -0300

    Alterado o tag

--- challenges/desafio3.js
+++ challenges/desafio3.js
@@ -1,16 +1,16 @@
 db.produtos.updateMany(
   {},
-  { $set: { avaliacao: NumberInt("0") } },
+  { $set: { avaliacao: NumberInt(0) } },
 );
 
 db.produtos.updateMany(
-  { tags: { $all: ["bovino"] } },
+  { tags: "bovino" },
   { $inc: { avaliacao: 5 } },
 );
 
 db.produtos.updateMany(
-  { tags: { $all: ["ave"] } },
-  { $inc: { $avaliacao: 3 } },
+  { tags: "ave" },
+  { $inc: { avaliacao: 3 } },
 );
 
 db.produtos.find(
