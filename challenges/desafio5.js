57439a4f413e5c3ad26de0a325ceed7686ef5ef4
Samuel <samuelpp.s@hotmail.com>
2021-02-27 22:41:13 -0300

    desafios até o 10 prontos

---
+++ challenges/desafio5.js
@@ -0,0 +1,22 @@
+/*
+Adicione ketchup aos ingredientes para todos os sanduíches menos o McChicken, garantindo que não haja duplicidade nos ingredientes.
+
+Para isso, escreva no arquivo desafio5.js duas queries, nesta ordem:
+
+Crie uma query que adicione ketchup aos ingredientes para todos os sanduíches menos o McChicken, garantindo que não haja duplicidade nos ingredientes.
+
+Crie uma query que retorne o nome e ingredientes de todos os documentos.
+*/
+
+db.produtos.update({ nome: { $not: { $regex: /McChicken/ } } }, {
+    $addToSet: { ingredientes: "ketchup" },
+  },
+  false,
+  true,
+);
+
+db.produtos.find({}, {
+    nome: true,
+    ingredientes: true,
+    _id: false,
+  });
