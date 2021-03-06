8e56d845248c562ab0fc43333b12d6051795f18c
Gabriel Luiz da Silva <58389773+gitgabrielluiz@users.noreply.github.com>
2021-03-06 18:20:46 -0300

    lint

--- challenges/desafio21.js
+++ challenges/desafio21.js
@@ -1,6 +1,6 @@
 db.produtos.find({ $expr: { $gt: ["$curtidas", "$vendidos"] } },
   {
-  nome: 1,
-  _id: 0,
-  }
+    nome: 1,
+    _id: 0,
+  },
 );
--- challenges/desafio22.js
+++ challenges/desafio22.js
@@ -3,5 +3,5 @@ db.produtos.find({ vendidos: { $mod: [5, 0] } },
     nome: 1,
     _id: 0,
     vendidos: 1,
-  }
+  },
 );
